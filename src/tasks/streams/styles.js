4bc66f19a0966639e27538bb73dd60be2030b7dd
James Leckenby <james.leckenby@paceit.co.uk>
2017-02-07 19:51:28 +0000

    Switch sass to use include paths

--- src/tasks/streams/styles.js
+++ src/tasks/streams/styles.js
@@ -22,10 +22,11 @@ module.exports = paths => () => gulp.src(paths.src)
     // Compile
     .pipe(sourcemaps.init())
     .pipe(sass({
-        importer: importer({
-            basedir: process.cwd()
-        })
-    }))
+        includePaths: [
+            path.join(process.cwd(), 'node_modules'),
+            process.cwd()
+        ]
+    ))
         .on('error', handleError)
     .pipe(sourcemaps.write())
 
